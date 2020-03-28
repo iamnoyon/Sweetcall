0ba74f856ac406563b6f84e412b86e6f9646a978
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2020-03-28 23:53:39 +0600

    Added Bottom Navigations and their activity and also set clicklisner

--- app/src/main/java/com/noyon/sweetcall/MainActivity.java
+++ app/src/main/java/com/noyon/sweetcall/MainActivity.java
@@ -1,26 +1,58 @@
 package com.noyon.sweetcall;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn;
+    BottomNavigationView navView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(i);
-            }
-        });
+        navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            switch (menuItem.getItemId()){
+                case R.id.navigation_home:
+                    Intent home = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(home);
+                    break;
+                case R.id.navigation_settings:
+                    Intent settings = new Intent(MainActivity.this, SettingsActivity.class);
+                    startActivity(settings);
+                    break;
+                case R.id.navigation_notifications:
+                    Intent notifications = new Intent(MainActivity.this, NotificationsActivity.class);
+                    startActivity(notifications);
+                    break;
+                case R.id.navigation_logout:
+                    FirebaseAuth.getInstance().signOut();
+                    Intent logout =  new Intent(MainActivity.this,RegistrationActivity.class);
+                    startActivity(logout);
+                    finish();
+                    break;
+            }
+            return true;
+        }
+    };
+
 }
